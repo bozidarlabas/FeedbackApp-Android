f701ef7ca8ff311bc73cb88453c3279a72bd09a0
kgzo <kagezero0@gmail.com>
2015-09-23 21:30:58 +0200

    PRojectModell added

---
+++ app/src/main/java/com/example/kanta/feedbackapp/mvp/models/ProjectModel.java
@@ -0,0 +1,47 @@
+package com.example.kanta.feedbackapp.mvp.models;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by boogiepop on 23.09.15..
+ */
+public class ProjectModel {
+
+    public static final String ID = "id";
+    public static final String NAME = "name";
+    public static final String RATING = "rating";
+
+    @SerializedName(ID)
+    private int id;
+
+    @SerializedName(NAME)
+    private String name;
+
+    @SerializedName(RATING)
+    private String rating;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+}
--- app/src/main/java/com/example/kanta/feedbackapp/service/RequestApi.java
+++ app/src/main/java/com/example/kanta/feedbackapp/service/RequestApi.java
@@ -3,7 +3,9 @@ package com.example.kanta.feedbackapp.service;
 import retrofit.Callback;
 import retrofit.http.Field;
 import retrofit.http.FormUrlEncoded;
+import retrofit.http.Multipart;
 import retrofit.http.POST;
+import retrofit.mime.TypedFile;
 
 /**
  * Created by kanta on 23.09.15..
@@ -28,5 +30,16 @@ public interface RequestApi {
     public void sendLoginRequest(
             @Field("password") String password,
             @Field("username") String username,
+            Callback<String> response);
+
+    @Multipart
+    @FormUrlEncoded
+    @POST("/WebDiP/2013_projekti/WebDiP2013_038/login.php")
+    public void sendAllProjects(
+            @Field("feedback") String feedback,
+            @Field("rating") float rating,
+            @Field("lat") String latitude,
+            @Field("long") String longitude,
+            @Field("attachment") TypedFile attachment,
             Callback<String> response);
 }
